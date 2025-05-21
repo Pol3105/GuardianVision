7991684c32c23cb5b1b3d9a47bc95c8c2efae629
Fransmm <fransmontesinosm@gmail.com>
2025-05-21 15:22:34 +0200

    Añadido lo que faltaba de busqueda

--- views/catalogo/objetos.php
+++ views/catalogo/objetos.php
@@ -16,41 +16,44 @@
         include_once __DIR__ . "/../templates/alertas.php";
     ?>
 
-    <?php
-        foreach ($productos as $producto):
-    ?>
+    <?php if(empty($productos)): ?>
+        <p>No se encontraron productos.</p>
+    <?php else: ?>
+        <?php
+            foreach ($productos as $producto):
+        ?>
 
-    <div class="divisor">
-        <div class="texto">
-            <h2>
-                <?php echo $producto->nombre ?>
-            </h2>
-            <p>
-                <?php echo $producto->descripcion ?>
-            </p>
+        <div class="divisor">
+            <div class="texto">
+                <h2>
+                    <?php echo $producto->nombre ?>
+                </h2>
+                <p>
+                    <?php echo $producto->descripcion ?>
+                </p>
 
-            <p>
-              <strong><?php echo $producto->precio ?></strong> $$
-            </p>
-            <div>
-                <form class="formulario" action="catalogo" method="post">
-                    <input type="hidden" name="idCliente" value="<?php echo isset($_SESSION['usuario_id']) ?>">
-                    <input type="hidden" name="idProducto" value="<?php echo $producto->cod_producto ?>">
-                    <input type="hidden" name="Cantidad" value="1">
+                <p>
+                <strong><?php echo $producto->precio ?></strong> $$
+                </p>
+                <div>
+                    <form class="formulario" action="catalogo" method="post">
+                        <input type="hidden" name="idCliente" value="<?php echo isset($_SESSION['usuario_id']) ?>">
+                        <input type="hidden" name="idProducto" value="<?php echo $producto->cod_producto ?>">
+                        <input type="hidden" name="Cantidad" value="1">
 
-                    <input class="boton-compra" type="submit" value="Agregar al carrito">
-                </form>
+                        <input class="boton-compra" type="submit" value="Agregar al carrito">
+                    </form>
+                </div>
+            </div>
+            <div class="contenedor-imagen imagen_<?php echo $producto->imagen?>">      
             </div>
         </div>
-        <div class="contenedor-imagen imagen_<?php echo $producto->imagen?>">      
-        </div>
-    </div>
 
-    <?php
+        <?php
 
-        endforeach;
-    
-    ?>
-
+            endforeach;
+        
+        ?>
+    <?php endif; ?>
 
 </div>
